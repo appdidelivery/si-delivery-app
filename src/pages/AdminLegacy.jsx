7b895858d965d16738573883a47edd5da66c3ff6
Diego <appdidelivery@gmail.com>
2026-04-22 15:21:53 -0300

    att google maps

--- src/pages/AdminLegacy.jsx
+++ src/pages/AdminLegacy.jsx
@@ -9476,7 +9476,7 @@ Esta ação registrará o prêmio como "pago" e não pode ser desfeita.`;
                     </motion.div>
                 )}
             </AnimatePresence>
-            {/* --- FIM: MODAL DE IA PARA COPY DE PROMOÇÕES --- */}
+            {/* --- FIM: MODAL DE IA PARA COPY 2DE PROMOÇÕES --- */}
 
             {/* --- INÍCIO: MODAL DE IA PARA COPY DE PROMOÇÕES --- */}
             <AnimatePresence>
